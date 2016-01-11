5e8709f90faf762225ff5c27de3639b22d0e240c
Vivek Kumar Bansal <vkb0310@gmail.com>
2016-01-11 17:04:45 +0530

    add jsx-control-statements plugin

---
+++ jsx-control-statements.js
@@ -0,0 +1,19 @@
+"use strict";
+
+module.exports = {
+    "plugins": [
+        "jsx-control-statements"
+    ],
+    "ecmaFeatures": {
+        "jsx": true
+    },
+    "rules": {
+        "jsx-control-statements/jsx-for-require-each": 2,
+        "jsx-control-statements/jsx-for-require-index": 2,
+        "jsx-control-statements/jsx-for-require-of": 2,
+        "jsx-control-statements/jsx-for-single-child": 2,
+        "jsx-control-statements/jsx-if-require-condition": 2,
+        "jsx-control-statements/jsx-if-single-child": 2,
+        "jsx-control-statements/jsx-use-if-tag": 1
+    }
+};
