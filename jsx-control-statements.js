6a21ea9153af09900fa4bd6bba0070b6707f2b0f
Vivek Kumar Bansal <vkb0310@gmail.com>
2016-04-18 08:02:46 +0530

    update jsx-control-statement rules

--- jsx-control-statements.js
+++ jsx-control-statements.js
@@ -10,13 +10,15 @@ module.exports = {
     "rules": {
         "jsx-control-statements/jsx-choose-not-empty": 1,
         "jsx-control-statements/jsx-for-require-each": 1,
-        "jsx-control-statements/jsx-for-require-index": 1,
         "jsx-control-statements/jsx-for-require-of": 1,
-        "jsx-control-statements/jsx-for-single-child": 0,
         "jsx-control-statements/jsx-if-require-condition": 1,
-        "jsx-control-statements/jsx-if-single-child": 0,
         "jsx-control-statements/jsx-otherwise-once-last": 1,
         "jsx-control-statements/jsx-use-if-tag": 1,
-        "jsx-control-statements/jsx-when-require-condition": 1
+        "jsx-control-statements/jsx-when-require-condition": 1,
+        "jsx-control-statements/jsx-jcs-no-undef": 1,
+        "no-undef": 0 // Replace this with jsx-jcs-no-undef
+    },
+    env: {
+        "jsx-control-statements/jsx-control-statements": true
     }
 };
