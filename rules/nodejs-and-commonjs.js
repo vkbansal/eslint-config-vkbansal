dcaa19f246906080fcba8e02272da80165321f98
Vivek Kumar Bansal <vkb0310@gmail.com>
2016-04-18 08:01:34 +0530

    Split rules

---
+++ rules/nodejs-and-commonjs.js
@@ -0,0 +1,12 @@
+module.exports = {
+    "callback-return": "off",
+    "global-require": "warn",
+    "handle-callback-err": "off",
+    "no-mixed-requires": ["warn", {"grouping": true}],
+    "no-new-require": "off",
+    "no-path-concat": "error",
+    "no-process-env": "off",
+    "no-process-exit": "off",
+    "no-restricted-modules": "off",
+    "no-sync": "off"
+};
---
+++ rules/variables.js
@@ -0,0 +1,14 @@
+module.exports = {
+    "init-declarations": "off",
+    "no-catch-shadow": "off",
+    "no-delete-var": "error",
+    "no-label-var": "error",
+    "no-restricted-globals": "off",
+    "no-shadow-restricted-names": "off",
+    "no-shadow": ["off", {"builtinGlobals": true, "hoist": "all"}],
+    "no-undef": "error",
+    "no-undef-init": "error",
+    "no-undefined": "error",
+    "no-unused-vars": "error",
+    "no-use-before-define": "error"
+}
