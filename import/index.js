2450d31201a66b2a85ad689564c5faa8ef221055
Vivek Kumar Bansal <vkb0310@gmail.com>
2019-06-18 15:15:13 +0530

    fix: remove react config from import rules

--- import/index.js
+++ import/index.js
@@ -2,7 +2,6 @@ module.exports = {
     plugins: ['import'],
     extends: [
         'plugin:import/typescript',
-        'plugin:import/react',
         ...['./helpful-warnings', './module-systems', './static-analysis', './style-guide'].map(
             require.resolve
         )
